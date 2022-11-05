931b733e6664af0061c4c22e23feefb4f58b612f
YUUKIToriyama <github@toriyama.dev>
2022-11-06 06:33:57 +0900

    CLIを修正
    出力ファイル名・画像サイズ・マルチスレッドの有効無効をコマンドライン引数から指定できるようにした

--- src/bin.rs
+++ src/bin.rs
@@ -1,16 +1,35 @@
-use std::path::Path;
+use clap::Parser;
 
-use image::{ColorType, ImageFormat};
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+    /// マルチスレッドを有効にする
+    #[arg(long = "multi-thread")]
+    enable_multi_thread: Option<bool>,
+    /// 横の長さを指定する(px)
+    #[arg(long)]
+    width: u32,
+    /// 縦の長さを指定する(px)
+    #[arg(long)]
+    height: u32,
+    /// 出力先
+    #[arg(long = "out-file")]
+    file_name: String,
+}
 
-use draw_julia::{
-    complex::Complex,
-    julia::get_julia_set,
-    util::{Bound, RADIUS_OF_CONVERGENCE},
-};
-
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    let width = 500;
-    let height = 500;
+    use image::{ColorType, ImageFormat};
+    use std::path::Path;
+
+    use draw_julia::{
+        complex::Complex,
+        julia::{get_julia_set_multi_thread, get_julia_set_single_thread},
+        util::{Bound, RADIUS_OF_CONVERGENCE},
+    };
+
+    let args = Cli::parse();
+
     let bound = Bound {
         north: RADIUS_OF_CONVERGENCE,
         south: -RADIUS_OF_CONVERGENCE,
@@ -21,12 +40,15 @@ fn main() {
         re: -0.15,
         im: 0.65,
     };
-    let image_buffer = get_julia_set(width, height, bound, c);
+    let image_buffer = match args.enable_multi_thread.unwrap_or(false) {
+        true => get_julia_set_multi_thread(args.width, args.height, bound, c),
+        false => get_julia_set_single_thread(args.width, args.height, bound, c),
+    };
     image::save_buffer_with_format(
-        Path::new("./generated.png"),
+        Path::new(&args.file_name),
         &image_buffer,
-        width,
-        height,
+        args.width,
+        args.height,
         ColorType::Rgba8,
         ImageFormat::Png,
     )
