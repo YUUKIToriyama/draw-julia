55b01d977557bfc322484bae0a4459349a5e4f48
YUUKIToriyama <github@toriyama.dev>
2022-11-06 00:20:04 +0900

    Arc/Mutexを使って並列処理するように書き直した

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use std::ops::Add;
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::wasm_bindgen;
@@ -112,38 +114,51 @@ impl JuliaSet {
     /// ジュリア集合の画像データを生成する
     fn get_julia_set(canvas_width: u32, canvas_height: u32, bound: Bound, c: Complex) -> Vec<u8> {
         let data_size = canvas_height * canvas_width * 4;
-        let mut data: Vec<u8> = vec![0; data_size as usize];
+        let data = Arc::new(Mutex::new(vec![0; data_size as usize]));
 
         let scale_x = (bound.east - bound.west).abs() / (canvas_width as f64);
         let scale_y = (bound.north - bound.south).abs() / (canvas_height as f64);
 
+        let mut handles = vec![];
+
         for y in 0..canvas_height {
             for x in 0..canvas_width {
-                // 初期値を設定
-                let z0 = Complex {
-                    re: bound.west + (x as f64) * scale_x,
-                    im: bound.south + (y as f64) * scale_y,
-                };
-                let i = (y * canvas_width + x) as usize;
-                match Self::calculate_sequence_limit(z0, c) {
-                    // 収束する場合は黒(#000000)
-                    SequenceLimit::Convergence => {
-                        data[4 * i] = 0; // R
-                        data[4 * i + 1] = 0; // G
-                        data[4 * i + 2] = 0; // B
-                        data[4 * i + 3] = 0; // A
+                let data = Arc::clone(&data);
+                let handle = thread::spawn(move || {
+                    // 初期値を設定
+                    let z0 = Complex {
+                        re: bound.west + (x as f64) * scale_x,
+                        im: bound.south + (y as f64) * scale_y,
+                    };
+                    // 収束・発散を計算
+                    let result = Self::calculate_sequence_limit(z0, c);
+                    // 画像データに書き込む
+                    let mut data = data.lock().unwrap();
+                    let i = (y * canvas_width + x) as usize;
+                    match result {
+                        // 収束する場合は黒(#000000)
+                        SequenceLimit::Convergence => {
+                            (*data)[4 * i] = 0; // R
+                            (*data)[4 * i + 1] = 0; // G
+                            (*data)[4 * i + 2] = 0; // B
+                            (*data)[4 * i + 3] = 0; // A
+                        }
+                        // 発散する場合は適当な色に着色する
+                        SequenceLimit::Divergence(count) => {
+                            (*data)[4 * i] = (255 - count / 2) as u8; // R
+                            (*data)[4 * i + 1] = (255 - count / 4) as u8; // G
+                            (*data)[4 * i + 2] = (255 - count / 6) as u8; // B
+                            (*data)[4 * i + 3] = 255; // A
+                        }
                     }
-                    // 発散する場合は適当な色に着色する
-                    SequenceLimit::Divergence(count) => {
-                        data[4 * i] = (255 - count / 2) as u8; // R
-                        data[4 * i + 1] = (255 - count / 4) as u8; // G
-                        data[4 * i + 2] = (255 - count / 6) as u8; // B
-                        data[4 * i + 3] = 255; // A
-                    }
-                }
+                });
+                handles.push(handle);
             }
         }
-        return data;
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        return Arc::try_unwrap(data).unwrap().lock().unwrap().to_vec();
     }
 
     /// 与えられた初期値に対して数列が収束するか発散するかを計算する
